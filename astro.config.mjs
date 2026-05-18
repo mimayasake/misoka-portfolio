07551fdb93863728e1759ce7a6d1a20ce8011047
mimayasake <enone999@gmail.com>
2026-05-19 00:22:28 +0900

    fix: astro.config.mjs for cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import { processAnnotations } from 'astro/runtime/client/dev-toolbar/apps/audit/annotations.js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mimayasake.github.io',
-  base: '/misoka-portfolio/',
+  base: process.env.CF_PAGES === '1' ? '/' : '/misoka-portfolio/',
   integrations: [react()],
 
   vite: {
